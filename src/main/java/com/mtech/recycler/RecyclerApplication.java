3ffacf730ae6b457d866d6b997a4b086140a7b4d
Raymond Htet <htetthurazaw@gmail.com>
2023-03-14 22:28:04 +0800

    implement backbone of the project

---
+++ src/main/java/com/mtech/recycler/RecyclerApplication.java
@@ -0,0 +1,13 @@
+package com.mtech.recycler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclerApplication.class, args);
+	}
+
+}
