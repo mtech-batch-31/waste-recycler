6f094c68d9bd12a4a4c8ab6af547e86c41e6f9fc
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-04-07 18:54:33 +0800

    Resolve CORS error when frontend calls Login API

--- src/main/java/com/mtech/recycler/model/LoginRequest.java
+++ src/main/java/com/mtech/recycler/model/LoginRequest.java
@@ -1,11 +1,29 @@
 package com.mtech.recycler.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
+
+    public LoginRequest() {}
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
+
