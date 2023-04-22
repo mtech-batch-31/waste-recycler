e96afed9399e014e9131cbe84e65b5d9fd4972c9
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-04-22 20:33:11 +0800

    Bug fix in JwtTokenProvider

--- src/main/java/com/mtech/recycler/config/JwtTokenProvider.java
+++ src/main/java/com/mtech/recycler/config/JwtTokenProvider.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.joda.time.Instant;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +23,10 @@ import java.util.function.Function;
 public class JwtTokenProvider {
     @Value("${app.jwtSecret}")
     private String jwtSecret;
+    @Autowired
+    public JwtTokenProvider(@Value("${app.jwtSecret}") String jwtSecret) {
+        this.jwtSecret = jwtSecret;
+    }
     @Value("${app.jwtExpirationInMinutes}")
     private int jwtExpirationInMinutes;
 
